d1ea881d6372de25c7b16a2f1ee05e68f66df464
Paula Meng <0065paula@gmail.com>
2021-03-31 11:14:42 +0800

    Add youtube link to footer, and change social links in footer to icons

--- components/footer.js
+++ components/footer.js
@@ -17,8 +17,27 @@ export const Footer = () => {
             <a className="footerLink">Contact Us</a>
           </Link>
           <span className="text-gray-300"> | </span>
-          <a className="footerLink" href="https://join.slack.com/t/slack-vcm1551/shared_invite/zt-nx33ud5h-~0D_MD5kL0sUrwslB~KF_A" target="_blank">Slack</a>
-          <a className="footerLink" href="https://twitter.com/iomeshhq" target="_blank">Twitter</a>
+          <a className="hover:opacity-70" href="https://join.slack.com/t/slack-vcm1551/shared_invite/zt-nx33ud5h-~0D_MD5kL0sUrwslB~KF_A" target="_blank">
+            <Image
+              src="/Slack_footer.svg"
+              width={16}
+              height={16}
+            />
+          </a>
+          <a className="hover:opacity-70" href="https://twitter.com/iomeshhq" target="_blank">
+            <Image
+              src="/Twitter_footer.svg"
+              width={16}
+              height={16}
+            />
+          </a>
+          <a className="hover:opacity-70" href="https://www.youtube.com/channel/UCJV59ZDxjm822LK_oUhi7qA" target="_blank">
+            <Image
+              src="/YouTube_footer.svg"
+              width={16}
+              height={16}
+            />
+          </a>
         </div>
       </footer>
     )
